9a585b63b766f22df44e5c5df0aa72497e264526
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2026-04-05 23:39:01 +0530

    Add FB Messenger SSL Bypass script
    This script implements SSL bypass for FB Messenger by hooking into the SSL verification process and modifying the behavior of certificate verification methods.

---
+++ SSL/FBMessengerSSL.js
@@ -0,0 +1,155 @@
+/*
+ FB Messenger SSL Bypass
+*/
+
+console.log("\n[+] FB SSL Bypass Loaded\n");
+console.log("[+] Arch:", Process.arch);
+
+// Custom params
+const MODE = {
+    FBCertificateVerifier: true,
+    NetworkSecurityTrustManager: true
+}
+
+// load Java Library
+const loadJava = (name) => {
+    try {
+        const myClass = Java.use(name);
+        console.log("[+] Class:", name);
+        return myClass;
+    } catch(e) {;
+        console.log("[✘] Error loading:", name, "-", e.message);
+        return null;
+    }
+};
+
+// load Binary ( https://codeshare.frida.re/@Eltion/facebook-ssl-pinning-bypass/ )
+function waitForModules(moduleName) {
+    return new Promise(resolve => {
+        const interval = setInterval(() => {
+            const module = Process.findModuleByName(moduleName);
+            if (module != null) {
+                console.log("[+] Module:", moduleName);
+                console.log("[+] Path:", module.path);
+                clearInterval(interval);
+                resolve(module);
+            }
+        }, 300);
+    });
+}
+
+
+// hook proxygen SSLVerification
+function hook_proxygen_SSLVerification(library) {
+
+    const functionName = "X509_verify_cert"
+
+    try {
+    
+        if (!library) {
+            console.log("[!] Library not found:", library);
+            return;
+        }
+
+        // Find by export
+        var target = library.enumerateExports().find(
+            exports => exports.name === functionName
+        );
+
+        if (!target) {
+            console.log(`[✘] ${functionName} not found\n`);
+            return;
+        }
+
+        console.log("[+] Found:", target.name);
+        console.log("[+] Address:", target.address);
+
+        Interceptor.attach(target.address, {
+            onLeave: retvalue => retvalue.replace(ptr(1))
+        });
+
+        console.log(`[✓] Patched: ${functionName}\n`);
+
+    } catch (e) {
+        console.log("[✘] Error in proxygen SSLVerification:", e.message);
+    }
+}
+
+
+/* /data/data/{packageName}/lib-compressed/*.so */
+waitForModules("libcoldstart.so").then((lib) => {
+    hook_proxygen_SSLVerification(lib);
+});
+
+
+setTimeout(function() {
+
+    console.log("\n--- Meta Bypass Loaded ---\n");
+
+    if (Java.available) {
+        console.log("[*] Java available");
+        Java.perform(function() {
+
+            // if SSL Unpinning by Native then may be skip CertificateVerifier
+            if (MODE.FBCertificateVerifier) {
+
+                const CertificateVerifier = loadJava("com.facebook.mobilenetwork.internal.certificateverifier.CertificateVerifier");
+
+                if (CertificateVerifier) {
+
+                    // verify()
+                    try {
+                        CertificateVerifier.verify.overload(
+                            '[Ljava.security.cert.X509Certificate;',
+                            'java.lang.String',
+                            'boolean'
+                        ).implementation = function(certChain, hostname, someBoolean) {
+                            console.log(`[✓] Bypassed CertificateVerifier.verify(certChain, "${hostname}", ${someBoolean})`);
+                        };
+                        console.log("[✓] CertificateVerifier [verify] hook applied");
+                    } catch (e) {
+                        console.log(`[✘] CertificateVerifier [verify] ${e}`);
+                    }
+
+                    // verifyWithProofOfPossession
+                    try {
+                        CertificateVerifier.verifyWithProofOfPossession.overload(
+                            '[[B',
+                            'java.lang.String',
+                            'boolean',
+                            'int',
+                            '[B',
+                            '[B'
+                        ).implementation = function(certBytes, hostname, flag, intVal, arr1, arr2) {
+                            console.log(`[✓] Bypassed verifyWithProofOfPossession ➢ ${hostname}`);
+                        };
+                        console.log("[✓] CertificateVerifier [verifyWithProofOfPossession] hook applied");
+                    } catch (e) {
+                        console.log(`[✘] CertificateVerifier [verifyWithProofOfPossession] ${e}`);
+                    }
+                }
+            }
+
+            // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/security/net/config/NetworkSecurityTrustManager.java
+            if (MODE.NetworkSecurityTrustManager) {
+
+                const NSTrustManager = loadJava('android.security.net.config.NetworkSecurityTrustManager');
+
+                if (NSTrustManager) {
+                    try {
+                        NSTrustManager.isPinningEnforced.overload('java.util.List').implementation = function (chain) {
+                            console.log('[✓] Bypassed isPinningEnforced');
+                            return false;
+                        };
+                        console.log('[✓] NetworkSecurityTrustManager [isPinningEnforced] hook applied');
+                    } catch (e) {
+                        console.log(`[✘] NetworkSecurityTrustManager [isPinningEnforced] ${e}`);
+                    }
+                }
+            }
+        });
+    } else {
+        console.log(`[ᵎ!ᵎ] Java unavailable`);
+    }
+    console.log("\n---- Capturing setup completed ----\n");
+}, 0);
