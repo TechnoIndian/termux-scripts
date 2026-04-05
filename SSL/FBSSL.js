836970fd80f374ff2657013c386dba419b38f715
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2026-04-05 12:33:44 +0530

    Improve SSL Bypass with better error handling
    Refactor SSL bypass logic to enhance module loading and error handling.

--- SSL/FBSSL.js
+++ SSL/FBSSL.js
@@ -1,30 +1,55 @@
 /*
  FB SSL Bypass
 */
+
 console.log("\n[+] FB SSL Bypass Loaded\n");
 console.log("[+] Arch:", Process.arch);
 
-var moduleName = "libcoldstart.so";  // /data/data/lib-compressed/*.so
-var maxRetries = 10;
-var retryCount = 0;
+// Custom params
+const MODE = {
+    FBCertificateVerifier: true,
+    NetworkSecurityTrustManager: true
+}
+
+// load Java Library
+const loadJava = (name) => {
+    try {
+        const myClass = Java.use(name);
+        console.log("[+] Class:", name);
+        return myClass;
+    } catch (e) {
+        console.log("[✘] Error loading", name + ":", e.message);
+        return null;
+    }
+};
+
+// load Binary
+const loadSo = (name) => {
+    try {
+        const module = Process.findModuleByName(name);
+        if (module) {
+            console.log("[+] Module:", name);
+            console.log("[+] Path:", module.path);
+            return module;
+        }
+        console.log("[ᵎ!ᵎ] Module not loaded:", name);
+        return null;
+    } catch (e) {
+        console.log("[✘] Error:", e.message);
+        return null;
+    }
+};
 
 function hookSSL() {
-
-    var module = Process.findModuleByName(moduleName);
-
-    if (!module) {
-        retryCount++;
-        if (retryCount <= maxRetries) {
-            console.log(`[!] Waiting for ${moduleName} (${retryCount}/${maxRetries})`);
-            setTimeout(hookSSL, 1000);
-        }
-        return;
-    }
-
-    // Find by export
-    var exports = module.enumerateExports();
-
     try {
+
+        /* /data/data/com.facebook.katana/lib-compressed/libcoldstart.so */
+        var module = loadSo("libcoldstart.so");
+        if (!module) return;
+
+        // Find by export
+        var exports = module.enumerateExports();
+
         for (var i = 0; i < exports.length; i++) {
             var exp = exports[i];
 
@@ -32,7 +57,6 @@ function hookSSL() {
                 var addr = exp.address;
                 var symbol = exp.name;
 
-                console.log(`[+] Module: ${moduleName}`);
                 console.log(`[+] Symbol: ${symbol.substring(0, 80)}...`);
                 console.log(`[+] Address: ${addr}`);
 
@@ -43,53 +67,82 @@ function hookSSL() {
                     }
                 });
 
-                console.log(`[+] Status: PATCHED ✓\n`);
+                console.log(`[✓] Status: PATCHED\n`);
                 return;
             }
         }
     } catch (e) {
-        console.error("[-] verifyWithMetrics not found, Error:");
-        console.error(e);
+        console.log("[✘] Error in hookSSL:", e.message);
     }
 }
 
-hookSSL();
+setTimeout(function() {
+    hookSSL();
+    console.log("\n--- Meta Bypass Loaded ---\n");
+    if (Java.available) {
+        console.log("[*] Java available");
+        Java.perform(function() {
 
-Java.perform(function() {
-    console.log("--- Meta Bypass Loaded ---");
-    console.log("--- Targeting the core 'verify' method ---");
-    try {
-        const CertificateVerifier = Java.use("com.facebook.mobilenetwork.internal.certificateverifier.CertificateVerifier");
+            // if SSL Unpinning by Native then may be skip CertificateVerifier
+            if (MODE.FBCertificateVerifier) {
 
-        // verify()
-        CertificateVerifier.verify.overload(
-            '[Ljava.security.cert.X509Certificate;',
-            'java.lang.String',
-            'boolean'
-        ).implementation = function(certChain, hostname, someBoolean) {
-            console.log(`[+] Bypassed CertificateVerifier.verify(certChain, "${hostname}", ${someBoolean}). Certificate chain is now trusted.`);
-            return;
-        };
+                const CertificateVerifier = loadJava("com.facebook.mobilenetwork.internal.certificateverifier.CertificateVerifier");
 
-        console.log("[+] Hook on CertificateVerifier.verify with correct signature is active.");
+                if (CertificateVerifier) {
 
-        // verifyWithProofOfPossession
-        CertificateVerifier.verifyWithProofOfPossession.overload(
-            '[[B',
-            'java.lang.String',
-            'boolean',
-            'int',
-            '[B',
-            '[B'
-        ).implementation = function(certBytes, hostname, flag, intVal, arr1, arr2) {
-            console.log(`[+] verifyWithProofOfPossession bypassed -> ${hostname}`);
-            return;
-        };
+                    // verify()
+                    try {
+                        CertificateVerifier.verify.overload(
+                            '[Ljava.security.cert.X509Certificate;',
+                            'java.lang.String',
+                            'boolean'
+                        ).implementation = function(certChain, hostname, someBoolean) {
+                            console.log(`[✓] Bypassed CertificateVerifier.verify(certChain, "${hostname}", ${someBoolean})`);
+                        };
+                        console.log("[✓] CertificateVerifier [verify] hook applied");
+                    } catch (e) {
+                        console.log(`[✘] CertificateVerifier [verify] ${e}`);
+                    }
 
-        console.log("[+] Hooked: verifyWithProofOfPossession");
-    
-    } catch (e) {
-        console.error("[-] Failed to hook:");
-        console.error(e);
+                    // verifyWithProofOfPossession
+                    try {
+                        CertificateVerifier.verifyWithProofOfPossession.overload(
+                            '[[B',
+                            'java.lang.String',
+                            'boolean',
+                            'int',
+                            '[B',
+                            '[B'
+                        ).implementation = function(certBytes, hostname, flag, intVal, arr1, arr2) {
+                            console.log(`[✓] Bypassed verifyWithProofOfPossession ➢ ${hostname}`);
+                        };
+                        console.log("[✓] CertificateVerifier [verifyWithProofOfPossession] hook applied");
+                    } catch (e) {
+                        console.log(`[✘] CertificateVerifier [verifyWithProofOfPossession] ${e}`);
+                    }
+                }
+            }
+
+            // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/security/net/config/NetworkSecurityTrustManager.java
+            if (MODE.NetworkSecurityTrustManager) {
+
+                const NSTrustManager = loadJava('android.security.net.config.NetworkSecurityTrustManager');
+
+                if (NSTrustManager) {
+                    try {
+                        NSTrustManager.isPinningEnforced.overload('java.util.List').implementation = function (chain) {
+                            console.log('[✓] Bypassed isPinningEnforced');
+                            return false;
+                        };
+                        console.log('[✓] NetworkSecurityTrustManager [isPinningEnforced] hook applied');
+                    } catch (e) {
+                        console.log(`[✘] NetworkSecurityTrustManager [isPinningEnforced] ${e}`);
+                    }
+                }
+            }
+        });
+    } else {
+        console.log(`[ᵎ!ᵎ] Java unavailable`);
     }
-});
+    console.log("\n---- Capturing setup completed ----\n");
+}, 0);
