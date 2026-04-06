46b9537e8ff58bb885f6c7f47e42e2f7a1ec850c
Techno India <133761719+TechnoIndian@users.noreply.github.com>
2026-04-06 09:47:45 +0530

    Improve SSL bypass with additional trust managers
    Added support for SSLContext and ConscryptTrustManagerImpl to enhance SSL bypass functionality.

--- SSL/FBMessengerSSL.js
+++ SSL/FBMessengerSSL.js
@@ -8,20 +8,19 @@ console.log("[+] Arch:", Process.arch);
 // Custom params
 const MODE = {
     FBCertificateVerifier: true,
-    NetworkSecurityTrustManager: true
+    NetworkSecurityTrustManager: true,
+    ConscryptTrustManagerImpl: true,
+    SSLContext: true
 }
 
 // load Java Library
-const loadJava = (name) => {
+const loadJava = (library) => {
     try {
-        const myClass = Java.use(name);
-        console.log("[+] Class:", name);
-        return myClass;
-    } catch(e) {;
-        console.log("[✘] Error loading:", name, "-", e.message);
-        return null;
+        return Java.use(library);
+    } catch (e) {
+        return undefined;
     }
-};
+}
 
 // load Binary ( https://codeshare.frida.re/@Eltion/facebook-ssl-pinning-bypass/ )
 function waitForModules(moduleName) {
@@ -82,74 +81,159 @@ waitForModules("libcoldstart.so").then((lib) => {
 });
 
 
-setTimeout(function() {
+setTimeout(function () {
 
     console.log("\n--- Meta Bypass Loaded ---\n");
 
     if (Java.available) {
-        console.log("[*] Java available");
-        Java.perform(function() {
+
+        console.log("\n[*] Java available\n");
+
+        Java.perform(function () {
 
             // if SSL Unpinning by Native then may be skip CertificateVerifier
             if (MODE.FBCertificateVerifier) {
 
+                console.log("\n[+] Facebook Custom CertificateVerifier\n");
+
                 const CertificateVerifier = loadJava("com.facebook.mobilenetwork.internal.certificateverifier.CertificateVerifier");
 
-                if (CertificateVerifier) {
+                // Bypass CertificateVerifier.verify
+                try {
+                    CertificateVerifier.verify.overload(
+                        "[Ljava.security.cert.X509Certificate;",
+                        "java.lang.String",
+                        "boolean"
+                    ).implementation = function (certChain, hostname, someBoolean) {
+                        console.log(`[✓] Bypassed CertificateVerifier.verify(certChain, "${hostname}", ${someBoolean})`);
+                    };
+                } catch (e) {
+                    console.log("[✘] CertificateVerifier [verify] not found");
+                }
 
-                    // verify()
-                    try {
-                        CertificateVerifier.verify.overload(
-                            '[Ljava.security.cert.X509Certificate;',
-                            'java.lang.String',
-                            'boolean'
-                        ).implementation = function(certChain, hostname, someBoolean) {
-                            console.log(`[✓] Bypassed CertificateVerifier.verify(certChain, "${hostname}", ${someBoolean})`);
-                        };
-                        console.log("[✓] CertificateVerifier [verify] hook applied");
-                    } catch (e) {
-                        console.log(`[✘] CertificateVerifier [verify] ${e}`);
-                    }
-
-                    // verifyWithProofOfPossession
-                    try {
-                        CertificateVerifier.verifyWithProofOfPossession.overload(
-                            '[[B',
-                            'java.lang.String',
-                            'boolean',
-                            'int',
-                            '[B',
-                            '[B'
-                        ).implementation = function(certBytes, hostname, flag, intVal, arr1, arr2) {
-                            console.log(`[✓] Bypassed verifyWithProofOfPossession ➢ ${hostname}`);
-                        };
-                        console.log("[✓] CertificateVerifier [verifyWithProofOfPossession] hook applied");
-                    } catch (e) {
-                        console.log(`[✘] CertificateVerifier [verifyWithProofOfPossession] ${e}`);
-                    }
+                // Bypass CertificateVerifier.verifyWithProofOfPossession
+                try {
+                    CertificateVerifier.verifyWithProofOfPossession.overload(
+                        "[[B",
+                        "java.lang.String",
+                        "boolean",
+                        "int",
+                        "[B",
+                        "[B"
+                    ).implementation = function (certBytes, hostname, flag, intVal, arr1, arr2) {
+                        console.log(`[✓] Bypassed verifyWithProofOfPossession ➢ ${hostname}`);
+                    };
+                } catch (e) {
+                      console.log("[✘] CertificateVerifier [verifyWithProofOfPossession] not found");
                 }
             }
 
-            // https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/security/net/config/NetworkSecurityTrustManager.java
+
+            // NetworkSecurityTrustManager ( https://android.googlesource.com/platform/frameworks/base/+/master/core/java/android/security/net/config/NetworkSecurityTrustManager.java ) //
             if (MODE.NetworkSecurityTrustManager) {
 
-                const NSTrustManager = loadJava('android.security.net.config.NetworkSecurityTrustManager');
+                console.log("\n[+] NetworkSecurityTrustManager\n");
 
-                if (NSTrustManager) {
-                    try {
-                        NSTrustManager.isPinningEnforced.overload('java.util.List').implementation = function (chain) {
-                            console.log('[✓] Bypassed isPinningEnforced');
-                            return false;
-                        };
-                        console.log('[✓] NetworkSecurityTrustManager [isPinningEnforced] hook applied');
-                    } catch (e) {
-                        console.log(`[✘] NetworkSecurityTrustManager [isPinningEnforced] ${e}`);
-                    }
+                try {
+                    const NSTrustManager = Java.use("android.security.net.config.NetworkSecurityTrustManager");
+
+                    NSTrustManager.isPinningEnforced.overload("java.util.List").implementation = function (chain) {
+                        console.log("[✓] Bypassed isPinningEnforced");
+                        return false;
+                    };
+                } catch (e) {
+                    console.log("[✘] NetworkSecurityTrustManager [isPinningEnforced] not found");
+                }
+            }
+
+
+            // TrustManager (Android < 7) //
+            if (MODE.SSLContext) {
+
+                console.log("\n[+] X509TrustManager [SSLContext]\n");
+
+                try {
+                    const X509TrustManager = Java.use("javax.net.ssl.X509TrustManager");
+                    const SSLContext = Java.use("javax.net.ssl.SSLContext");
+
+                    const TrustManager = Java.registerClass({
+                        // Implement a custom TrustManager
+                        name: "dev.asd.test.TrustManager",
+                        implements: [X509TrustManager],
+                        methods: {
+                            checkClientTrusted: function (chain, authType) {},
+                            checkServerTrusted: function (chain, authType) {},
+                            getAcceptedIssuers: function () { return []; }
+                        }
+                    });
+
+                    // Prepare the TrustManager array to pass to SSLContext.init()
+                    const TrustManagers = [TrustManager.$new()];
+                    // Get a handle on the init() on the SSLContext class
+                    const SSLContext_init = SSLContext.init.overload("[Ljavax.net.ssl.KeyManager;", "[Ljavax.net.ssl.TrustManager;", "java.security.SecureRandom");
+                    // Override the init method, specifying the custom TrustManager
+                    SSLContext_init.implementation = function (keyManager, trustManager, secureRandom) {
+                        console.log("[✓] TrustManager [SSLContext] (Android < 7)");
+                        SSLContext_init.call(this, keyManager, TrustManagers, secureRandom);
+                    };
+                } catch (e) {
+                    console.log("[✘] TrustManager [SSLContext] (Android < 7) not found");
+                }
+            }
+
+
+            // TrustManagerImpl (Android > 7) //
+            if (MODE.ConscryptTrustManagerImpl) {
+
+                console.log("\n[+] ConscryptTrustManagerImpl\n");
+
+                const TrustManagerImpl = loadJava("com.android.org.conscrypt.TrustManagerImpl");
+
+                // Bypass TrustManagerImpl.checkTrustedRecursive (Android > 7) {1}
+                try {
+                    const ArrayList = Java.use("java.util.ArrayList");
+                    TrustManagerImpl.checkTrustedRecursive.implementation = function (certs, ocspData, tlsSctData, host, clientAuth, untrustedChain, trustAnchorChain, used) {
+                        console.log(`[✓] Bypassing TrustManagerImpl [TrustedRecursive] (Android > 7): ${host}, ${untrustedChain}`);
+                        return ArrayList.$new();
+                    };
+                } catch (e) {
+                    console.log("[✘] TrustManagerImpl [checkTrustedRecursive] (Android > 7) not found");
+                }
+
+                // Bypass TrustManagerImpl.checkTrustedRecursive (Android > 7) {2}
+                try {
+                    const ArrayList = Java.use("java.util.ArrayList");
+                    TrustManagerImpl.checkTrustedRecursive.implementation = function (certs, host, clientAuth, untrustedChain, trustAnchorChain, used) {
+                        console.log(`[✓] Bypassing TrustManagerImpl [checkTrustedRecursive] (Android > 7): ${host}, ${untrustedChain}`);
+                        return ArrayList.$new();
+                    };
+                } catch (e) {
+                    console.log("[✘] TrustManagerImpl [checkTrustedRecursive] (Android > 7) not found");
+                }
+
+                // Bypass TrustManagerImpl.verifyChain (Android > 7) {1} (probably no more necessary)
+                try {
+                    TrustManagerImpl.verifyChain.implementation = function (untrustedChain, trustAnchorChain, host, clientAuth, ocspData, tlsSctData) {
+                        console.log(`[✓] TrustManagerImpl [verifyChain] (Android > 7): ${host}, ${untrustedChain}`);;
+                        return untrustedChain;
+                    };
+                } catch (e) {
+                    console.log("[✘] TrustManagerImpl [verifyChain] (Android > 7) not found");
+                }
+
+                // Bypass TrustManagerImpl.verifyChain (Android > 7) {1} (probably no more necessary)
+                try {
+                    TrustManagerImpl.verifyChain.implementation = function (untrustedChain, trustAnchorChain, host, clientAuth) {
+                        console.log(`[✓] TrustManagerImpl [verifyChain] (Android > 7): ${host}, ${untrustedChain}`);
+                        return untrustedChain;
+                    };
+                } catch (e) {
+                    console.log("[✘] TrustManagerImpl [verifyChain] (Android > 7) not found");
                 }
             }
         });
     } else {
-        console.log(`[ᵎ!ᵎ] Java unavailable`);
+        console.log("\n[!] Java unavailable\n");
     }
     console.log("\n---- Capturing setup completed ----\n");
 }, 0);
